57b48a32c6eb5648412346db6f87a2a481e36a85
Adrian <74800016+acombs801@users.noreply.github.com>
2021-02-02 22:57:35 -0800

    managed to get local storage to display on page, events now persist

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-//When the planner is opened Todays date is shown on top
+//When the planner is opened Todays date and time is shown on top
 
 //Under the date are time blocks for each hour of the work day'9-5'
 
@@ -10,38 +10,54 @@
 
 var todaysDate = moment().format('LLL');
     console.log(todaysDate);
+var event = localStorage.getItem("Event");
+    
 
+    
 $("#currentDay").append(todaysDate);
 
 var container = $(".container");
 
-var times = ["9am", "10am","11am","12pm","1pm","2pm","3pm","4pm","5pm"];
+var times = ["9am", "10am","11am","12pm","1pm","2pm","3pm","4pm", "5pm"];
 
 for (let i = 0; i < times.length; i++) {
     var row = $("<div>").addClass("row");
     var timeBlock = $("<div>").addClass("time-block col-1").text(times[i]);
-    var textArea = $("<textarea>").addClass("col-10");
+    var textArea = $("<textarea>").addClass("col-10").attr("id",times[i]);
     var button = $("<button>").addClass("saveBtn col-1");
     container.append(row);
     row.append(timeBlock, textArea, button);
 }
 
+
+
 $(".saveBtn").on("click", function(){
-    var textContent = $(this).prev().val();
+    var eventContent = $(this).prev().val();
     var timeEl = $(this).prev().prev().text();
-    localStorage.setItem("savedText", JSON.stringify(textContent));
-    localStorage.setItem("savedTime", JSON.stringify(timeEl));
+
+    localStorage.setItem(JSON.stringify(timeEl).replace(/["']/g, ""), JSON.stringify(eventContent));
+    // localStorage.setItem("Time", JSON.stringify(timeEl));
+
 
     console.log(timeEl);
-    console.log(textContent);
+    console.log(eventContent);
+  
 });
 
-// function GetSavedData() {
-//     window.localStorage.getItem("textContent");
-//     window.localStorage.getItem("timeEl");
-    
+   function renderSavedData() {
+    //  var saved9am = JSON.parse(localStorage.getItem("9am"));
+    //  var saved10am = JSON.parse(localStorage.getItem("10am"));
+    for (let i = 0; i < localStorage.length; i++) {
+        console.log(document.getElementById(localStorage.key(i)));
+        if(document.getElementById(localStorage.key(i)) !== null){
+        
+        document.getElementById(localStorage.key(i)).value = localStorage.getItem(localStorage.key(i)).replace(/["']/g, "");
+        }
+    };
 
-// };
+};
+   renderSavedData(); 
+ 
 
-// GetSavedData();
-// console.log(GetSavedData);
+
+
